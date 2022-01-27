f44504239fea6a6a10547c24c638b18cdcc217a9
Odette Salvoro <odette@Odettes-MacBook-Pro.local>
2022-01-27 10:59:36 -0500

    imported restaurants.json

---
+++ index.js
@@ -0,0 +1,11 @@
+//import our restaurants
+const restaurants = require('./restaurants.json');
+
+console.log(restaurants[0].name)
+
+//connect to firestore
+
+//create a collection called "restaurant"
+
+// add each restaurant
+
